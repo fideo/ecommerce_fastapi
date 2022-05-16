338959db906361407fecbb50997e2a9095b60fa6
Leonardo Guanco <leo.guanco@gmail.com>
2022-05-15 21:51:56 -0300

    Agregado modelo de producto

--- sql_app/models.py
+++ sql_app/models.py
@@ -2,11 +2,12 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship
 from database import Base
 
+
 class Usuario(Base):
     __tablename__ = "usuarios"
-    
-    usuario_id = Column(Integer,primary_key=True,index=True)
-    correo_de_usuario = Column(String,unique=True)
+
+    usuario_id = Column(Integer, primary_key=True, index=True)
+    correo_de_usuario = Column(String, unique=True)
     contraseña_encriptada = Column(String)
     pais = Column(String)
     esta_activo = Column(Boolean)
@@ -16,31 +17,37 @@ class Usuario(Base):
 class Vendedor(Base):
     __tablename__ = "vendedores"
 
-    vendedor_id = Column(Integer,primary_key=True,index=True)
-    correo_de_vendedor = Column(String,unique=True)
-    contraseña_encriptada = Column(String)    
+    vendedor_id = Column(Integer, primary_key=True, index=True)
+    correo_de_vendedor = Column(String, unique=True)
+    contraseña_encriptada = Column(String)
     pais = Column(String)
     ciudad = Column(String)
     esta_activo = Column(Boolean)
-    productos_publicados = relationship("Producto",
-                                back_populates="vendedor_del_producto")
+    productos_publicados = relationship(
+        "Producto", back_populates="vendedor_del_producto"
+    )
 
 
 class Producto(Base):
     __tablename__ = "productos"
 
-    producto_id = Column(Integer,primary_key=True,index=True)
+    producto_id = Column(Integer, primary_key=True, index=True)
     nombre_producto = Column(String)
     fecha_de_publicacion = Column(Date)
     numero_de_productos_subidos = Column(Integer)
     precio_unitario_de_producto = Column(Integer)
-    vendedor_del_producto_id = Column(Integer,ForeignKey("Vendedor.vendedor_id"))
-    vendedor_del_producto = relationship("Vendedor",
-                                back_populates="productos_publicados")
+    vendedor_del_producto_id = Column(Integer, ForeignKey("Vendedor.vendedor_id"))
+    vendedor_del_producto = relationship(
+        "Vendedor", back_populates="productos_publicados"
+    )
 
-#class Ventas(Base):
 
-#    __tablename__ = "ventas"
+class Ventas(Base):
+    __tablename__ = "ventas"
 
-#    pass
-    
+    venta_id = Column(Integer, primary_key=True, index=True)
+    fecha_de_venta = Column(Date)
+    numero_de_productos_comprados = Column(Integer)
+    precio_total_de_venta = Column(Integer)
+    producto_id = Column(Integer, ForeignKey("Producto.producto_id"))
+    producto = relationship("Producto", back_populates="ventas_realizadas")
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,40 +1,52 @@
-from typing import Optional
+from typing import List, Optional
 from pydantic import BaseModel
+
 
 class UsuarioBase(BaseModel):
     correo_de_vendedor: str
 
+
 class UsuarioCreate(UsuarioBase):
     password: str
 
+
 class Usuario(UsuarioBase):
     usuario_id: int
-    esta_activo: bool    
-    class Config:
-        orm_mode = True
-
-class VendedorBase(BaseModel):
-    correo_de_usuario: str
-
-class VendedorCreate(VendedoresBase):
-    password: str
-
-class Vendedor(VendedoresBase):
-    vendedor_id: int
     esta_activo: bool
-    productos_publicados: list[Producto] = []
+
     class Config:
         orm_mode = True
+
 
 class ProductoBase(BaseModel):
     titulo: str
     descripcion: Optional[str] = None
 
+
 class ProductoCreate(ProductoBase):
     pass
+
 
 class Producto(ProductoBase):
     producto_id: int
     vendedor_del_producto_id: int
+
     class Config:
-        orm_mode = True    
+        orm_mode = True
+
+
+class VendedorBase(BaseModel):
+    correo_de_usuario: str
+
+
+class VendedorCreate(VendedorBase):
+    password: str
+
+
+class Vendedor(VendedorBase):
+    vendedor_id: int
+    esta_activo: bool
+    productos_publicados: List[Producto] = []
+
+    class Config:
+        orm_mode = True
