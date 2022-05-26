3fc2bd654a94e5008258786e8be9b276891921ba
Federico Mazzei <federicomazzei@gmail.com>
2022-05-26 15:56:28 -0300

    sigo la documentacion fastAPI sql_app/main.py

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -60,30 +60,35 @@ def buscar_producto(db: Session, palabra_clave:str):
 def subir_producto_a_carrito():
     pass
 
-def crear_categoria(db: Session, categoria:schemas.CategoriaCreate):
-    
-    db_categoria = models.Categoria(
+def obtener_categorias(db:Session):
+    categoria = db.query(models.Categoria).all()
+    return categoria
+
+def crear_categorias(categoria:schemas.Categoria, db:Session):
+    categoria = models.Categoria(
         nombre_categoria = categoria.nombre_categoria,
         descripcion = categoria.descripcion,
         esta_activo = categoria.esta_activo
     )
-
-    db.add(db_categoria)
+    db.add(categoria)
     db.commit()
-    db.refresh(db_categoria)
-    return db_categoria
+    db.refresh(categoria)
+    return categoria
 
-def eliminar_categoria(db: Session, categoria_id: int):
-    categoria_a_eliminar = db.query(models.Categoria).filter(
-                                models.Categoria.categoria_id == categoria_id).first()
-    
-    if categoria_a_eliminar is None:
-        return
-    
-    db.delete(categoria_a_eliminar)
-    db.commit()
 
-def modificar_categoria(db: Session, categoria_id: int, categoria:schemas.CategoriaCreate):
-    db.query(models.Categoria).filter(
-                                models.Categoria.categoria_id == categoria_id).update(categoria)
+def actualizar_categorias(categoria_id:int, categoria_actualizada:schemas.ActualizarCategoria, db:Session):
+    categoria = db.query(models.Categoria).filter_by(categoria_id=categoria_id).first()
+    print(categoria_id)
+    categoria.nombre_categoria = categoria_actualizada.nombre_categoria
+    categoria.descripcion = categoria_actualizada.descripcion
+    categoria.esta_activo = categoria_actualizada.esta_activo
     db.commit()
+    db.refresh(categoria)
+    return categoria
+
+def eliminar_categorias(categoria_id:int, db:Session):
+    categoria = db.query(models.Categoria).filter_by(categoria_id=categoria_id).first()
+    db.delete(categoria)
+    db.commit()
+    mensaje= schemas.EliminarCategoria(mensaje="La categoria "+ str(categoria_id) +" ha sido eliminada correctamente")
+    return mensaje
