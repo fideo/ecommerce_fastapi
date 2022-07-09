e75168a767c54b4efd9dbbced4931f44c7e5b39b
Federico Mazzei <federicomazzei@gmail.com>
2022-07-09 13:38:04 -0300

    subo mi base

--- app/sql_app/database.py
+++ app/sql_app/database.py
@@ -10,7 +10,7 @@ postgres_host = os.environ.get("POSTGRES_HOST")
 postgres_db = os.environ.get("POSTGRES_DB")
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./ecommerce_fastapi.db"
-#QLALCHEMY_DATABASE_URL = f"postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/ecommerce_fastapi"
+#SQLALCHEMY_DATABASE_URL = f"postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/ecommerce_fastapi"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
